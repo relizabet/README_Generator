df4bd3a3b74995a826c22e382c521b362980d1e3
Rachel Campbell <rach.eliza.campbell@icloud.com>
2020-08-18 18:32:06 -0400

    questions array

--- index.js
+++ index.js
@@ -1,14 +1,80 @@
 const fs = require("fs");
 const inquirer = require("inquirer");
+const util = require("util");
+// const generateMarkdown = require("ugenerateMarkdown.js");
+
+const writeFileAsync = util.promisify(fs.writeFile);
 
 // array of questions for user
-const questions = [{}];
+const questions = [
+  {
+    type: "input",
+    name: "title",
+    message: "What is your projects title?",
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "Enter a description of your project.",
+  },
+  {
+    type: "input",
+    name: "installation",
+    message: "How should your project be installed?",
+  },
+  {
+    type: "input",
+    name: "usage",
+    message: "How is your application used?",
+  },
+  {
+    type: "input",
+    name: "contributing",
+    message: "How should other developers contribute to your project?",
+  },
+  {
+    type: "input",
+    name: "tests",
+    message: "What tests should be run on this project",
+  },
+  {
+    type: "list",
+    name: "license",
+    message: "Please choose a license:",
+    choices: [
+      "MIT",
+      "GNU Lesser General Public License v3.0",
+      "Mozilla Public License 2.0",
+      "GNU Affero General Public License v3.0",
+      "The Unlicense",
+      "Apache License 2.0",
+      "GNU General Public License v3.0",
+    ],
+  },
+];
 
 // function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data, err) {
+  try {
+    writeFileAsync();
+  } catch (err) {
+    console.log(err);
+  }
+}
 
 // function to initialize program
-function init() {}
+function init() {
+  return inquirer.prompt(questions).then((answers) => {
+    console.log(answers);
+  });
+}
 
-// function call to initialize program
 init();
+
+writeToFile("readme.md", generateMarkdown(answers));
+
+// Game Plan
+// maybe remove return (try both ways)
+// writeToFile('readme.md', generateMarkdown(answers))
+// write out writetofile
+// finish the
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,3 @@
-const fs = require("fs");
-const inquirer = require("inquirer");
-
 // function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
