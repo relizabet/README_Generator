58a60d7c87283cf9384f9bc59cd1ed9f92e3231c
Rachel Campbell <rach.eliza.campbell@icloud.com>
2020-08-21 07:41:32 -0400

    run with links

--- readme_0.md
+++ readme_0.md
@@ -1,42 +1,43 @@
 # README_Generator
 
-  [![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0) [![GitHub issues](https://img.shields.io/github/issues/relizabet/README_Generator.svg)](https://GitHub.com/relizabet/README_Generator/issues/)
+[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0) [![GitHub issues](https://img.shields.io/github/issues/aeopkaa/README_Generator.svg)](https://GitHub.com/aeopkaa/README_Generator/issues/) [![GitHub issues-closed](https://img.shields.io/github/issues-closed/aeopkaa/README_Generator.svg)](https://GitHub.com/aeopkaa/README_Generator/issues?q=is%3Aissue+is%3Aclosed)
 
-  ## Description
+## Description
 
-  A node.js readme generator
+A node.js readme generator
 
-  ## Table of Contents
-  
-  - [About the Project](#about-the-project)
-  - [Usage](#usage)
-  - [Contributing](#contributing)
-  - [Testing](#testing)
-  - [License](#license)
-  - [Contact](#contact)
-  
-  ## Installation
+## Table of Contents
 
-  Project should be run through your command line
+- [About the Project](#about-the-project)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [Testing](#testing)
+- [License](#license)
+- [Contact](#contact)
 
-  ## Usage
-  
-  After installing the application answer the prompts 
-    
-  ## License
-  
-  Mozilla Public License 2.0
+## Installation
 
-  ## Contributing
-  
-  Create a branch and submit a pull request to be reviewed
-  
-  ## Tests
-  
-  No tests have been created for this application at this time
+kaj ;kajkl fja;el akljd fa;lej alkjfd a
 
-  ## Questions
-  
-  Please reach out to me directly via my email address
-  - [relizabet](https://github.com/relizabet)
-  - For additional questions please email me at: rachel@relizabeth.com
+## Usage
+
+a;jkd a;kdjf a;kdjf a;ldkfj a;
+
+## License
+
+[Mozilla Public License 2.0](https://choosealicense.com/licenses/mpl-2.0/)
+
+## Contributing
+
+aldkfaldkfa ldkfa' ;dk akldfa'l;dfka'ldkfadskfa's;dfasdfaoepakl faoeka fka dk
+
+## Tests
+
+a'ofka'eo kfald kfaoekfaldkf aoef adkfoe alkdf aoek a
+
+## Questions
+
+aodkfao eakf a'eopa kaeopfk
+
+- Github: [aeopkaa](https://github.com/aeopkaa)
+- Email: akefpaoekfa@goapk.com
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -2,27 +2,35 @@
 function generateMarkdown(data) {
   const license = data.license;
   let licenseBadge;
+  let licenseLink;
 
   if (license === "MIT") {
     licenseBadge = `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
-  `;
+    `;
+    licenseLink = "https://choosealicense.com/licenses/mit/";
   } else if (license === "GNU Lesser General Public License v3.0") {
     licenseBadge = `[![License: LGPL v3](https://img.shields.io/badge/License-LGPL%20v3-blue.svg)](https://www.gnu.org/licenses/lgpl-3.0)`;
+    licenseLink = "https://choosealicense.com/licenses/lgpl-3.0/";
   } else if (license === "Mozilla Public License 2.0") {
     licenseBadge = `[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)`;
+    licenseLink = "https://choosealicense.com/licenses/mpl-2.0/";
   } else if (license === "GNU Affero General Public License v3.0") {
     licenseBadge = `[![License: AGPL v3](https://img.shields.io/badge/License-AGPL%20v3-blue.svg)](https://www.gnu.org/licenses/agpl-3.0)`;
+    licenseLink = "https://choosealicense.com/licenses/agpl-3.0/";
   } else if (license === "The Unlicense") {
     licenseBadge = `[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)`;
+    licenseLink = "https://choosealicense.com/licenses/unlicense/";
   } else if (license === "Apache License 2.0") {
     licenseBadge = `[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`;
+    licenseLink = "https://choosealicense.com/licenses/apache-2.0/";
   } else if (license === "GNU General Public License v3.0") {
     licenseBadge = `[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`;
+    licenseLink = "https://choosealicense.com/licenses/gpl-3.0/";
   }
 
   return `# ${data.title}
 
-  ${licenseBadge} [![GitHub issues](https://img.shields.io/github/issues/${data.username}/${data.title}.svg)](https://GitHub.com/${data.username}/${data.title}/issues/)
+  ${licenseBadge} [![GitHub issues](https://img.shields.io/github/issues/${data.username}/${data.title}.svg)](https://GitHub.com/${data.username}/${data.title}/issues/) [![GitHub issues-closed](https://img.shields.io/github/issues-closed/${data.username}/${data.title}.svg)](https://GitHub.com/${data.username}/${data.title}/issues?q=is%3Aissue+is%3Aclosed)
 
   ## Description
 
@@ -47,7 +55,7 @@ function generateMarkdown(data) {
     
   ## License
   
-  ${data.license}
+  [${data.license}](${licenseLink})
 
   ## Contributing
   
@@ -60,8 +68,8 @@ function generateMarkdown(data) {
   ## Questions
   
   ${data.questions}
-  - [${data.username}](https://github.com/${data.username})
-  - For additional questions please email me at: ${data.email}
+  - Github: [${data.username}](https://github.com/${data.username})
+  - Email: ${data.email}
 `;
 }
 
